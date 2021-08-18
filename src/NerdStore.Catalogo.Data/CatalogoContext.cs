f086a75f7e026dc7c244a2be512429fb720cd76b
Pedro Carvalho <ppscarvalho@gmail.com>
2021-08-17 23:05:56 -0300

    commit inicial

---
+++ src/NerdStore.Catalogo.Data/CatalogoContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Data
+{
+    public class CatalogoContext : DbContext, IUnitOfWork
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options)
+            : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+            //    e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            //    property.Relational().ColumnType = "varchar(100)";
+
+            //modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+
+        public Task<bool> Commit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/NerdStore.Catalogo.Domain/EstoqueServiceBase.cs
@@ -0,0 +1,7 @@
+﻿namespace NerdStore.Catalogo.Domain
+{
+    public class EstoqueServiceBase
+    {
+        private readonly IProdutoRepository _produtoRepository;
+    }
+}
